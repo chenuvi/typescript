05e4911adc4fdc2c09c852f7ef5c089697552e25
chenuvi <chenuvi@outlook.com>
2024-02-22 22:22:51 +0800

    Feat: HandShake

--- exercises/practice/secret-handshake/secret-handshake.ts
+++ exercises/practice/secret-handshake/secret-handshake.ts
@@ -1,23 +1,23 @@
 class HandShake {
   private readonly secret: number;
-
   constructor(secret: number) {
     this.secret = secret;
   }
-
   static actions: Map<number, string> = new Map([
     [0b00001, "wink"],
     [0b00010, "double blink"],
     [0b00100, "close your eyes"],
     [0b01000, "jump"],
   ]);
-
   static reverse = 0b10000;
-
   commands(): string[] {
     const commands: string[] = [];
-
-    return commands;
+    for (const [flag, action] of HandShake.actions) {
+      if (this.secret & flag) {
+        commands.push(action);
+      }
+    }
+    return this.secret & HandShake.reverse ? commands.reverse() : commands;
   }
 }
 
