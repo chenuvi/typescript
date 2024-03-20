183045fa45a22a9952226e3d2875ac8225965b08
chenuvi <chenuvi@outlook.com>
2024-03-20 23:49:17 +0800

    Feat: 二分算法

--- exercises/practice/binary-search/binary-search.ts
+++ exercises/practice/binary-search/binary-search.ts
@@ -1,3 +1,18 @@
-export function find(haystack: unknown, needle: unknown): number | never {
-  throw new Error('Remove this statement and implement this function')
+export function find(haystack: number[], needle: number): number | never {
+  if (haystack.length === 0) {
+    throw new Error("Element not found in empty array");
+  }
+  let [left, right] = [0, haystack.length - 1];
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const curr = haystack[mid];
+    if (curr === needle) {
+      return mid;
+    } else if (curr < needle) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;  
+    }
+  }
+  throw new Error("Remove this statement and implement this function");
 }
